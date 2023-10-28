47aaf259d506bdd2f9c1f9d7661e6a33947c2fb6
TaeTaeKim <hc07car@gmail.com>
2023-10-28 12:49:30 +0900

    backend first commit

---
+++ src/test/java/droni/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package droni.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
