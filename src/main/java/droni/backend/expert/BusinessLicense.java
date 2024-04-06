b3ea8cab3c849ad3b0e15ea68c5dc429e3eb3955
김태윤 <mygod456@kakao.com>
2024-04-06 18:16:27 +0900

    <feat> : <사업자 등록증 entity mapping> [https://kakao-mygod456.atlassian.net/browse/DRONI-68?atlOrigin=eyJpIjoiYzRlNGVkNWUxMTA0NDRmNmI2OThjY2Q0NGY2ZjA4YzUiLCJwIjoiaiJ9]

---
+++ src/main/java/droni/backend/expert/BusinessLicense.java
@@ -0,0 +1,26 @@
+package droni.backend.expert;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class BusinessLicense {
+    @Id
+    private Integer licenseId;
+
+    @Column(nullable = false)
+    private String companyName;
+    @Column(nullable = false)
+    private String representative;
+    @Column(nullable = false)
+    private String companyAddress;
+    private String companyAddressDetail;
+
+
+}
