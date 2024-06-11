7120b7aaa5ac4bf28ec8da391aa990d74334d92b
김태윤 <mygod456@kakao.com>
2024-06-12 03:45:10 +0900

    <fix> : test 관련 설정 변경 및 token validation 테스트 코드 작성 [지라 이슈 링크]

--- src/test/java/droni/backend/BackendApplicationTests.java
+++ src/test/java/droni/backend/BackendApplicationTests.java
@@ -2,8 +2,10 @@ package droni.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({"local"})
 class BackendApplicationTests {
 
 	@Test
