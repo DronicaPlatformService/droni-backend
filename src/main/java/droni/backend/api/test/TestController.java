442510cbc7ce2b23d04e231e3ca90292f8d8c7cd
김태윤 <mygod456@kakao.com>
2024-06-11 20:42:08 +0900

    <fix> : test expired api 에서 406을 반환하도록 수정 [지라 이슈 링크]

--- src/main/java/droni/backend/api/test/TestController.java
+++ src/main/java/droni/backend/api/test/TestController.java
@@ -1,8 +1,8 @@
 package droni.backend.api.test;
 
 import backend.generated_api.TestApi;
-import droni.backend.oauth2.execption.JwtExpiredException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +13,7 @@ public class TestController implements TestApi {
 
     @Override
     public ResponseEntity<String> expireTestGet() {
-        throw new JwtExpiredException("JWT is expired");
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE.value()).build();
     }
 
     @Override
