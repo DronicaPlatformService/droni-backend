5afde028d2e521a8b4d4d52e8d71b8d86163fbc1
김태윤 <mygod456@kakao.com>
2024-08-05 01:52:37 +0900

    DRONI-117 <feat> : 사용자의 채팅방 목록을 가져오는 api 추가 [지라 이슈 링크]

--- src/main/java/droni/backend/api/message/controller/ChatController.java
+++ src/main/java/droni/backend/api/message/controller/ChatController.java
@@ -3,6 +3,7 @@ package droni.backend.api.message.controller;
 import backend.generated_api.ChatApi;
 import backend.generated_model.ChatMessage;
 import backend.generated_model.CreateChatRequest;
+import backend.generated_model.UserChatroom;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/chat")
+@RequestMapping(value = "/chat")
 public class ChatController implements ChatApi {
+    @Override
+    public ResponseEntity<List<UserChatroom>> chatChatroomUserIdGet(Long userId) {
+        return ChatApi.super.chatChatroomUserIdGet(userId);
+    }
 
     @Override
     public ResponseEntity<List<ChatMessage>> chatHistoryChatroomIdGet(Long chatroomId) {
