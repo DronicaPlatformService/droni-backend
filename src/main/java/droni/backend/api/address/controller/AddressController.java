ee526ec52d719f51032654fc4a350cd0df3ce6f0
Jeongwoo Kim <corc411@gmail.com>
2025-08-07 16:25:40 +0900

    feat: `ResponseEntity` 제거 및 `ResponseStatus` 적용
    - `ResponseEntity`를 사용하지 않고도 응답 상태를 명시적으로 설정할 수 있도록 `@ResponseStatus` 어노테이션을 적용했습니다.
    - `@ResponseStatus(HttpStatus.CREATED)`를 통해 주소지 생성 시 201 Created를 반환하도록 했습니다.
    - 기존 `@GetMapping`의 경우 기본적으로 200 OK를 반환하므로 별도의 어노테이션을 추가하지 않았습니다.
    - `ResponseEntity`를 제거함에 따라 코드 가독성이 향상되었습니다.
    
    Refs: #45, #47

--- src/main/java/droni/backend/api/address/controller/AddressController.java
+++ src/main/java/droni/backend/api/address/controller/AddressController.java
@@ -11,7 +11,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,22 +27,23 @@ public class AddressController {
 
     @Operation(summary = "주소지 생성")
     @PostMapping
-    public ResponseEntity<AddressResponse> saveAddress(
+    @ResponseStatus(HttpStatus.CREATED)
+    public AddressResponse saveAddress(
             @AuthenticationPrincipal OAuth2UserPrincipal userPrincipal,
             @RequestBody AddressSaveRequest request) {
         DroniUser user = droniUserRepository.findByOauthId(userPrincipal.getOAuth2Id())
                 .orElseThrow(() -> new DroniNotFoundException(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."));
-        return ResponseEntity
-                .ok(AddressResponse.from(addressService.saveAddress(user.getUserId(), request)));
+        return AddressResponse.from(addressService.saveAddress(user.getUserId(), request));
     }
 
     @Operation(summary = "주소지 목록 조회")
     @GetMapping
-    public ResponseEntity<List<AddressResponse>> getUserAddresses(
+    public List<AddressResponse> getUserAddresses(
             @AuthenticationPrincipal OAuth2UserPrincipal userPrincipal) {
         DroniUser user = droniUserRepository.findByOauthId(userPrincipal.getOAuth2Id())
                 .orElseThrow(() -> new DroniNotFoundException(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."));
-        return ResponseEntity.ok(addressService.getUserAddresses(user.getUserId()).stream()
-                .map(AddressResponse::from).toList());
+        return addressService.getUserAddresses(user.getUserId()).stream()
+                .map(AddressResponse::from)
+                .toList();
     }
 }
