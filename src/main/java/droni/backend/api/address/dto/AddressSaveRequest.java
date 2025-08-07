2e48e28af12452d5847c96eb7df835d0ec76b2d5
Jeongwoo Kim <corc411@gmail.com>
2025-08-07 14:31:01 +0900

    feat(address): AddressSaveRequest에 @Setter 어노테이션 추가
    AddressSaveRequest DTO에 @Setter를 적용하여 테스트 및 서비스 계층에서 필드 값 설정이 가능하도록 개선하였습니다.
    
    Refs: #45, #47

--- src/main/java/droni/backend/api/address/dto/AddressSaveRequest.java
+++ src/main/java/droni/backend/api/address/dto/AddressSaveRequest.java
@@ -2,8 +2,10 @@ package droni.backend.api.address.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class AddressSaveRequest {
     private String addressName;
